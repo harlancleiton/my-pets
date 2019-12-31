9ec225ee683b7d554b9d553834ab9a8de4df8cdb
Harlan Cleiton <harlancleiton@gmail.com>
2019-12-31 13:26:41 -0300

    chore: Add NestConfig

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import Joi = require('@hapi/joi');
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test')
+          .default('development'),
+        PORT: Joi.number().default(3333),
+        APP_NAME: Joi.string().default('MyPets'),
+        APP_KEY: Joi.string().required(),
+        DB_CONNECTION: Joi.string().required(),
+        DB_HOST: Joi.string(),
+        DB_PORT: Joi.number(),
+        DB_USER: Joi.string(),
+        DB_PASSWORD: Joi.string(),
+        DB_DATABASE: Joi.string(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
